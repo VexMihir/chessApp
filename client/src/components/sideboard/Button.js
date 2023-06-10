d2d4534b46ce2f81a94372bcb2f16b46df495ab1
Jason Lai <59099867+jason0770@users.noreply.github.com>
2023-06-10 09:46:56 -0700

    Delete client directory

--- client/src/components/sideboard/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Button(props) {
-  return (
-    <>
-      <button className='sideboard__button sideboard__button_elevated'>{props.name}</button>
-    </>
-  )
-}
