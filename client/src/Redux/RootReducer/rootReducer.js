b451b24fce1f084a9b683ab1e19947a46393dca5
Jason Lai <59099867+jason0770@users.noreply.github.com>
2023-06-24 09:51:57 -0700

    Update rootReducer.js

--- client/src/Redux/RootReducer/rootReducer.js
+++ client/src/Redux/RootReducer/rootReducer.js
@@ -1,10 +1,11 @@
 import {combineReducers} from "@reduxjs/toolkit";
 import {pgnReducers} from "../Reducers/PGN_Reducers";
 import {prevGameQueryReducer} from "../Reducers/prevGameQuery_Reducers";
-
+import { FENReducers } from '../Reducers/FEN_Reducers'
 export const rootReducer = combineReducers(
     {
         PGNReducer: pgnReducers,
+        FENReducer: FENReducers,
         PrevGameQuery: prevGameQueryReducer
     }
 )
