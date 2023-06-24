b7ba12e1c14934e7a168c5dda82af937357842f8
Jason Lai <59099867+jason0770@users.noreply.github.com>
2023-06-24 09:58:03 -0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -14,11 +14,11 @@ import {mainStore} from "./Redux/Store/mainStore";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    //<React.StrictMode>
         <Provider store={mainStore}>
             <ChessApp />
         </Provider>
-    </React.StrictMode>
+    //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
