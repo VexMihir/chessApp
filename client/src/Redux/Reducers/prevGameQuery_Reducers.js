1a2e2671906f71f013149a78b517921f595c767a
Jason Lai <59099867+jason0770@users.noreply.github.com>
2023-07-12 23:31:50 -0700

    Delete client directory

--- client/src/Redux/Reducers/prevGameQuery_Reducers.js
+++
@@ -1,8 +0,0 @@
-import {demo_Obj, LOADOBJDATABASE} from "../String/prevGameView_INITIALSTATE";
-
-export function prevGameQueryReducer(state = demo_Obj, action) {
-    switch (action.type) {
-        case LOADOBJDATABASE: return action.payload
-        default: return state;
-    }
-}
