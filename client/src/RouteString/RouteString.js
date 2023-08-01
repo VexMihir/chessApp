7997540b728025c24b08b19d3039c3d08fa11d97
UBC Student <nguyenphuocdat0976@gmail.com>
2023-07-31 19:20:27 -0700

    typo

--- client/src/RouteString/RouteString.js
+++ client/src/RouteString/RouteString.js
@@ -2,4 +2,4 @@ export const playBackView = "/playbackView";
 export const normalView = "/playbackView/normalView";
 export const analysisView = "/playbackView/analysisView"
 export const joinPage = "/";
-export const queryPage = "/previousGameView/"
+export const queryPage = "/previousGameView/1"
