175fe5824f3bcfe1986f428e1f7f5abc0b30ceba
Dan Blustein <dansakai0@gmail.com>
2023-08-03 19:25:23 -0700

    fix double decrement

--- server/socket/handlers/handleJoinRoom.js
+++ server/socket/handlers/handleJoinRoom.js
@@ -1,91 +1,98 @@
 const { EVENTS } = require('../aliases');
-const {pushToMongoAndManageDB} = require('./pushToMongoAndManageDB');
+const { pushToMongoAndManageDB } = require('./pushToMongoAndManageDB');
 
 
 const handleJoinRoom = (io, socket, rooms, gameModel, gameSchema) => (roomNumber, username) => {
 
-    console.log("line 5", roomNumber, username);
+  console.log("line 5", roomNumber, username);
 
-    const WHITE = "White";
-    const BLACK = "Black";
+  const WHITE = "White";
+  const BLACK = "Black";
 
-    const startingColor = () => {
-        const randomizer = Math.floor(Math.random() * 2);
-        if (randomizer > 0.5) {
-          return BLACK;
-        } else {
-          return WHITE;
-        }
-      };
+  const startingColor = () => {
+    const randomizer = Math.floor(Math.random() * 2);
+    if (randomizer > 0.5) {
+      return BLACK;
+    } else {
+      return WHITE;
+    }
+  };
 
-      const getOppositeColor = (firstPlayerColor) =>{
-        if (firstPlayerColor == WHITE) {
-            return BLACK;
-        } else {
-            return WHITE;
-        }
-      };
+  const getOppositeColor = (firstPlayerColor) => {
+    if (firstPlayerColor == WHITE) {
+      return BLACK;
+    } else {
+      return WHITE;
+    }
+  };
 
-      const startTimer = (roomNumber) => {
-        const timer = setInterval(() => {
-          const currentPlayer = rooms[roomNumber].currentPlayer;
-          rooms[roomNumber].timers[currentPlayer]--;
-          io.to(roomNumber).emit('time update', rooms[roomNumber].timers);
+  const startTimer = (roomNumber) => {
+    if (rooms[roomNumber].timer) return;
 
-        // Check if a player's timer has run out
-        if (rooms[roomNumber].timers[currentPlayer] == 0 && rooms[roomNumber].players.length === 2) {
-          const winningColor = currentPlayer === rooms[roomNumber].players[0].id ? rooms[roomNumber].players[1].color : rooms[roomNumber].players[0].color;
-          io.to(roomNumber).emit('timeout', winningColor);
-          rooms[roomNumber].winner = winningColor + " wins by Timeout"; // other player wins
-          pushToMongoAndManageDB(rooms[roomNumber], gameSchema, gameModel);
-          clearInterval(rooms[roomNumber].timer);
-        }
+    const timer = setInterval(() => {
+      const currentPlayer = rooms[roomNumber].currentPlayer;
+      rooms[roomNumber].timers[currentPlayer]--;
+      io.to(roomNumber).emit('time update', rooms[roomNumber].timers);
+
+      // Check if a player's timer has run out
+      if (rooms[roomNumber].timers[currentPlayer] == 0 && rooms[roomNumber].players.length === 2) {
+        const winningColor = currentPlayer === rooms[roomNumber].players[0].id ? rooms[roomNumber].players[1].color : rooms[roomNumber].players[0].color;
+        io.to(roomNumber).emit('timeout', winningColor);
+        rooms[roomNumber].winner = winningColor + " wins by Timeout"; // other player wins
+        pushToMongoAndManageDB(rooms[roomNumber], gameSchema, gameModel);
+        clearInterval(rooms[roomNumber].timer);
+      }
       // Emit a socket event to notify the clients that the game is over
-        }, 1000);
-      
-        rooms[roomNumber].timer = timer;
-      };
+    }, 1000);
 
-    if (rooms[roomNumber]) {
-        if (rooms[roomNumber].players.length < 2) {
-            socket.join(roomNumber);
-            let color = startingColor();
-            if (rooms[roomNumber].players.length === 1) {
-                firstPlayerColor = rooms[roomNumber].players[0].color;
-                color = getOppositeColor(firstPlayerColor);
-            }
-            rooms[roomNumber].players.push({ id: socket.id, username: username, color: color });
-            if (rooms[roomNumber].players.length === 2) {
-              rooms[roomNumber].currentPlayer = rooms[roomNumber].players[0].color == WHITE ? rooms[roomNumber].players[0].id :rooms[roomNumber].players[1].id;
-              timers = rooms[roomNumber].timers
+    rooms[roomNumber].timer = timer;
+  };
 
+  if (rooms[roomNumber]) {
+    if (rooms[roomNumber].players.length < 2) {
+      socket.join(roomNumber);
+      let color = startingColor();
+      if (rooms[roomNumber].players.length === 1) {
+        firstPlayerColor = rooms[roomNumber].players[0].color;
+        color = getOppositeColor(firstPlayerColor);
+      }
+      rooms[roomNumber].players.push({ id: socket.id, username: username, color: color });
+      if (rooms[roomNumber].players.length === 2) {
+        rooms[roomNumber].currentPlayer = rooms[roomNumber].players[0].color == WHITE ? rooms[roomNumber].players[0].id : rooms[roomNumber].players[1].id;
+        if (!rooms[roomNumber].timers[rooms[roomNumber].players[0].id]) {
+          timers = rooms[roomNumber].timers;
 
-              // set both players' timers to 5 minutes
-              timers[rooms[roomNumber].players[0].id] = rooms[roomNumber].timeControl;
-              timers[rooms[roomNumber].players[1].id] = rooms[roomNumber].timeControl;
-              io.to(roomNumber).emit(EVENTS.START_GAME);
-              startTimer(roomNumber);
-
-            }
-        } else {
-            socket.emit(EVENTS.ROOM_FULL, roomNumber);
-            console.log(`User ${socket.id} attempted to join room ${roomNumber}, which is full`);
+          // set both players' timers to time control
+          timers[rooms[roomNumber].players[0].id] = rooms[roomNumber].timeControl;
+          timers[rooms[roomNumber].players[1].id] = rooms[roomNumber].timeControl;
         }
-    } else {
-        console.log(`Room ${roomNumber} does not exist`);
-    }
+        io.to(roomNumber).emit(EVENTS.START_GAME);
+        startTimer(roomNumber);
 
-    let userList = {}
-    if (rooms[roomNumber] !== undefined && rooms[roomNumber].players !== undefined && rooms[roomNumber].spectators !== undefined) { 
-      userList = {
-          players: rooms[roomNumber].players,
-          spectators: rooms[roomNumber].spectators
-      };
+      }
     } else {
-      // error handling...
-      // room does not exist
+      socket.emit(EVENTS.ROOM_FULL, roomNumber);
+      console.log(`User ${socket.id} attempted to join room ${roomNumber}, which is full`);
     }
-    io.to(roomNumber).emit(EVENTS.USER_LIST_UPDATE, userList);
+  } else {
+    console.log(`Room ${roomNumber} does not exist`);
+  }
+
+  let userList = {}
+  if (rooms[roomNumber] !== undefined && rooms[roomNumber].players !== undefined && rooms[roomNumber].spectators !== undefined) {
+    userList = {
+      players: rooms[roomNumber].players,
+      spectators: rooms[roomNumber].spectators
+    };
+  } else {
+    // error handling...
+    // room does not exist
+  }
+  io.to(roomNumber).emit(EVENTS.USER_LIST_UPDATE, userList);
 }
 
-module.exports = {handleJoinRoom};
+module.exports = { handleJoinRoom };
+
+
+
+
