c75c6267783c390d4f0d11f9024c230e1ce3a2da
UBC Student <nguyenphuocdat0976@gmail.com>
2023-08-07 10:16:28 -0700

    mini adjustment for pagination

--- client/src/components/PrevQueryPage/Pagnination/Pagination.js
+++ client/src/components/PrevQueryPage/Pagnination/Pagination.js
@@ -282,7 +282,7 @@ export function Pagination() {
                         {
                             return <li key={child+"list"}>
                                 <NavLink
-                                    className={"flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"}
+                                    className={"flex items-center no-underline justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"}
                                     to={"/previousGameView/" + child}
                                     key={Number(child)+"index"}
                                     onClick={
