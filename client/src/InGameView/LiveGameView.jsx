41137aacafcf243e1ddef3ddaad2969be3119ed7
Dan Blustein <dansakai0@gmail.com>
2023-08-11 02:32:04 -0700

    movable chess pieces

--- client/src/InGameView/LiveGameView.jsx
+++ client/src/InGameView/LiveGameView.jsx
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import Chessboard from 'chessboardjsx';
+import {Chess} from 'chess.js';
 import InfoPanel from './InfoPanel';
 import { color } from '../constants';
 
 const LiveGameView = () => {
   const [isLoading, setIsLoading] = useState(true);
+
+  // User related data
   const [timeForWhite, setTimeForWhite] = useState(231);
   const [timeForBlack, setTimeForBlack] = useState(190);
-  const [orientation, setOrientation] = useState(color.WHITE);
   const [usernameForWhite, setUserNameWhite] = useState("user_white");
   const [usernameForBlack, setUserNameBlack] = useState("user_black");
+  const [orientation, setOrientation] = useState(color.WHITE);
+  
+  // Chess logic related data
+  const [chess] = useState(new Chess()); // create a new chess.js instance
+  const [fen, setFen] = useState(chess.fen()); // use FEN for board position
 
   const connectToServerSocket = () => {
     // timeout as mock socket.io connect for 1 second
@@ -19,21 +26,24 @@ const LiveGameView = () => {
   };
 
   const handleDrop = ({ sourceSquare, targetSquare }) => {
-    console.log(`Moved piece from ${sourceSquare} to ${targetSquare}`);
+    const move = chess.move({
+      from: sourceSquare,
+      to: targetSquare
+    });
+
+    if (move) setFen(chess.fen());
   };
 
-  
-
   useEffect(() => {
-    connectToServerSocket(); // Call the function when the component mounts
-  }, []); // Empty dependency array to ensure it runs only once
+    connectToServerSocket();
+  }, []); 
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-900">
       <div className="flex flex-row items-center">
         <div className="bg-sky-950 border-2 border-gray-500 p-3 rounded-lg shadow-lg">
           <Chessboard
-            position="start"
+            position={fen}
             orientation={orientation}
             darkSquareStyle={{ backgroundColor: '#3F72AF' }}
             lightSquareStyle={{ backgroundColor: '#F9F7F7' }}
